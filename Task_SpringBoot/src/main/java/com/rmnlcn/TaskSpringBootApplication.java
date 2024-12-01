d4dddb96e174fc7fa20176b21e144375f387d42d
RmNLcN06 <romain.luciano@orange.fr>
2024-12-01 23:12:38 +0100

    Initial commit

---
+++ Task_SpringBoot/src/main/java/com/rmnlcn/TaskSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rmnlcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSpringBootApplication.class, args);
+	}
+
+}
